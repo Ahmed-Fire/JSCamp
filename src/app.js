98f465b5297e9627a6fa79f5b56367889646d08e
Ahmed Imad <ahmad.emad4@gmail.com>
2021-10-19 00:09:24 +0300

    First Lesson - 19.10.2021

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+
+//JS in not Type Safe!
+let dolarDun = 9.20
+let dolarBugun = 9.30
+{
+    let dolarDun = 9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun)
+
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","ozel konut kredileri"]
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
