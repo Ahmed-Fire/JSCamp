9f30c5ebf5079b18f02c6ea461d6d27ffe7cc9d4
Ahmed Imad <ahmad.emad4@gmail.com>
2021-12-04 00:24:55 +0300

    22 November 2021

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,10 +1,11 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
 import User from "../models/user.js"
+import Customer from "../models/customer.js"
 import UserService from "../services/userServices.js"
 
 console.log("User Component Loaded")
 
-let logger1 = new ElasticLogger()
+let logger1 = new MongoLogger()
 let userService = new UserService(logger1)
 
 let user1 = new User(1, "Ahmed", "Emad", "Irbil")
@@ -13,10 +14,28 @@ let user2 = new User(2, "Engin", "Demirog", "Ankara")
 userService.add(user1)
 userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+//console.log(userService.list())
+//console.log(userService.getById(2))
 
-userService.getById(1)
-userService.list()
+//userService.getById(1)
+//userService.list()
 
-console.log()
+let customer = {id:1, firstName:"Ahmed"}
+
+//prototyping
+customer.lastName = "Emad"
+
+console.log(customer.lastName)
+
+console.log("---------------------------")
+userService.load()
+
+
+let customerToAdd = new Customer(1, "Payam", "Faris", "Irbil", "21a")
+customerToAdd.type = "customer"
+
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employee)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,19 +1,115 @@
+import { users } from "../data/users.js";
+import DataError from "../models/dataError.js";
+
 export default class UserService {
-    constructor(loggerService){
-        this.users = []
-        this.loggerService = loggerService
+  constructor(loggerService) {
+    this.employee = [];
+    this.customers = [];
+    this.errors = [];
+    this.loggerService = loggerService;
+  }
+
+  load() {
+    for (const user of users) {
+      switch (user.type) {
+        case "customer":
+          if (!this.checkCustomerValidityForErrors(user)) {
+            this.customers.push(user);
+          }
+          break;
+        case "employee":
+          if (!this.checkEmployeeValidityForErrors(user)) {
+            this.employee.push(user);
+          }
+          break;
+        default:
+          this.errors.push(new DataError("Wrong user type", user));
+          break;
+      }
+    }
+  }
+
+  //formik-yup
+  checkCustomerValidityForErrors(user) {
+    let requiredFields = "id firstName lastName age city".split(" ");
+    let hasErrors = false;
+    for (const field of requiredFields) {
+      if (!user[field]) {
+        hasErrors = true;
+        this.errors.push(
+          new DataError(`Validation Problem. ${field} is required`, user)
+        );
+      }
     }
 
-    add(user){
-        this.users.push(user)
-        this.loggerService.log(user)
+    if (Number.isNaN(Number.parseInt(user.age))) {
+      hasErrors = true;
+      this.errors.push(
+        new DataError(`Validation Problem. ${user.age} is not a number`, user)
+      );
     }
 
-    list(){
-        return this.users
+    return hasErrors;
+  }
+
+  checkEmployeeValidityForErrors(user) {
+    let requiredFields = "id firstName lastName age city salary".split(" ");
+    let hasErrors = false;
+    for (const field of requiredFields) {
+      if (!user[field]) {
+        hasErrors = true;
+        this.errors.push(
+          new DataError(`Validation Problem. ${field} is required`, user)
+        );
+      }
     }
-    
-    getById(id){
-        return this.users.find(u=>u.id === id)
+
+    if (Number.isNaN(Number.parseInt(+user.age))) {
+      hasErrors = true;
+      this.errors.push(
+        new DataError(`Validation Problem. ${user.age} is not a number`, user)
+      );
     }
+
+    return hasErrors;
+  }
+
+  add(user) {
+    switch (user.type) {
+      case "customer":
+        if (!this.checkCustomerValidityForErrors(user)) {
+          this.customers.push(user);
+        }
+        break;
+      case "empolyee":
+        if (!this.checkEmployeeValidityForErrors(user)) {
+          this.employee.push(user);
+        }
+        break;
+      default:
+        this.errors.push(new DataError("This user can not be added. Wrong user type", user));
+        break;
+    }
+    this.loggerService.log(user);
+  }
+
+  listCustomers() {
+    return this.customers
+  }
+
+  getCustomerById(id) {
+    return this.customers.find(u=>u.id === id)
+  }
+
+  getCustomersSorted(){
+    return this.customers.sort((customer1, customer2)=>{
+      if(customer1.firstName>customer2.firstName){
+        return 1;
+      }else if(customer1.firstName===customer2.firstName){
+        return 0;
+      }else{
+        return -1
+      }
+    })
+  }
 }
